d10696db0d4b6c6450652dbaf658213305434a22
Paul Adolph <padolph@gmail.com>
2019-03-07 21:30:27 -0800

    completed week 9

---
+++ machine-learning-ex8/ex8/estimateGaussian.m
@@ -0,0 +1,41 @@
+function [mu sigma2] = estimateGaussian(X)
+%ESTIMATEGAUSSIAN This function estimates the parameters of a 
+%Gaussian distribution using the data in X
+%   [mu sigma2] = estimateGaussian(X), 
+%   The input X is the dataset with each n-dimensional data point in one row
+%   The output is an n-dimensional vector mu, the mean of the data set
+%   and the variances sigma^2, an n x 1 vector
+% 
+
+% Useful variables
+[m, n] = size(X);
+
+% You should return these values correctly
+mu = zeros(n, 1);
+sigma2 = zeros(n, 1);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the mean of the data and the variances
+%               In particular, mu(i) should contain the mean of
+%               the data for the i-th feature and sigma2(i)
+%               should contain variance of the i-th feature.
+%
+
+% cheat and use matlab functions
+
+mu = mean(X);
+
+% note that as noted in the PDF, matlab's var function uses 1/(m-1)
+% instead of 1/m as needed by this problem. So multiply var() by (m-1)
+% then divide by m to get the answer we want.
+sigma2 = (m-1)/m * var(X);
+
+
+
+
+
+
+% =============================================================
+
+
+end
